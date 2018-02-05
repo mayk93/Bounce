3758acd7b713cb36eac512895fd7b06f3d0fa733
michael <mihai@mandrescu.co>
2018-02-05 11:07:28 +0000

    Added basic unit tests.

--- Simple/bounce.js
+++ Simple/bounce.js
@@ -1,3 +1,39 @@
 /**
  * Created by michael on 05/02/2018.
  */
+
+/* Board class - holds information about the ball and the canvas where it is drawn */
+function Board() {
+    this.spawned = false;
+    this.current_ball_position_x = null;
+    this.current_ball_position_y = null;
+}
+Board.prototype.spawn = function (x, y) {
+    this.spawned = true;
+    this.current_ball_position_x = x;
+    this.current_ball_position_y = y;
+};
+
+/* Functions */
+function create_canvas() {
+    let new_canvas = document.createElement("canvas");
+
+    new_canvas.setAttribute("id", "bounce_canvas");
+    new_canvas.setAttribute("class", "canvas_class");
+
+    document.getElementById("main").appendChild(new_canvas);
+}
+
+function main() {
+    create_canvas();
+
+    let board = new Board();
+}
+
+if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", main, false);
+} else if (document.attachEvent) {
+    document.attachEvent("onreadystatechange", main);
+} else {
+    window.onload = main;
+}
--- Simple/tests/bounce_test.js
+++ Simple/tests/bounce_test.js
@@ -1,3 +1,43 @@
 /**
  * Created by michael on 05/02/2018.
  */
+
+describe("Canvas Creation", function () {
+    let main;
+    let canvas;
+
+    beforeEach(function () {
+        /* Delete the canvas if it exists */
+        main = document.getElementById("main");
+        canvas = document.getElementById("bounce_canvas");
+        if (canvas) {
+            canvas.parentNode.removeChild(canvas);
+        }
+    });
+    
+    it("should not have a canvas", function () {
+        /* If it has no children, it has no canvas child */
+        expect(main.childNodes.length).toEqual(0);
+        expect(document.getElementById("bounce_canvas")).toBeNull();
+    });
+
+    it("should have a canvas child after initialization", function () {
+        create_canvas();
+        expect(main.childNodes.length).toEqual(1);
+        expect(document.getElementById("bounce_canvas")).toBeDefined(null);
+    })
+});
+
+describe("Bounce", function () {
+    let board;
+
+    beforeEach(function () {
+        board = new Board();
+    });
+
+    it("should be in a uninitialized state", function () {
+        expect(board.spawned).toEqual(false);
+        expect(board.current_ball_position_x).toEqual(null);
+        expect(board.current_ball_position_y).toEqual(null);
+    });
+});
