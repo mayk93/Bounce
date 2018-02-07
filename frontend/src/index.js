b4bced975dfb81c5bc583791b7347c726d79ae5b
michael <mihai@mandrescu.co>
2018-02-07 23:51:20 +0000

    Building React version of the Bounce App.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,32 @@
+/*
+ Import order:
+
+ React
+ Redux
+ External libs / components
+ My libs / components
+ Actions
+ Functions and Constants
+ Style and CSS
+
+ Other
+ */
+
+/* React */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+
+/* Actual APP */
 import App from './App';
+import './style/css/index.css';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />
+    ,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
---
+++ frontend/src/style/js/styles.js
@@ -0,0 +1,20 @@
+/**
+ * Created by michael on 07/02/2018.
+ */
+
+export let outer_card_style = {
+    width: "90%",
+    marginTop: "1%",
+    marginLeft: "5%",
+    paddingBottom: "1%"
+};
+
+export let inner_card_style = {
+    width: "90%",
+    marginTop: "1%",
+    marginLeft: "5%"
+};
+
+export let text_style = {
+    paddingRight: "0px"
+};
